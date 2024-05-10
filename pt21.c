5ac200a18f248141fd7fcb519f76d12c65f28c53
Jean Macedo <jeancmacedo1@gmail.com>
2024-05-10 14:56:51 -0300

    Update pt21.c
    commit

---
+++ pt21.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+//functions
+
+void birthday(){
+   
+    printf("\nHappy birthday to you!");
+    printf("\nHappy birthday to you!");
+    printf("\nHappy birthday dear...YOU!");
+    printf("\nHappy birthday to you!\n");
+
+}
+
+int main(){
+
+    birthday();
+    birthday();
+    birthday();
+    birthday();
+
+    return 0;
+}
