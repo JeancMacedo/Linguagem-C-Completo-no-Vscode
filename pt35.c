399728a21b9142dcc26256b058a255073883abbc
Jean Macedo <jeancmacedo1@gmail.com>
2024-05-12 19:42:26 -0300

    new commit

--- pt35.c
+++ pt35.c
@@ -6,7 +6,7 @@ int main()
    char cars[][10] = {"Mustang","Corvette","Camaro"};
 
    //cars[0] = "Tesla";
-   strcpy(cars[4], "Tesla");
+   strcpy(cars[0], "Tesla");
 
    for(int i = 0; i < sizeof(cars)/sizeof(cars[0]); i++)
    {
